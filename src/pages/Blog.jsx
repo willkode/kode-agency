b236fe1bdf5b86b97fdec0547cfa1e36a32b3585
Will Kode <iamwillkode@gmail.com>
2025-11-24 21:08:02 +0000

    File changes

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
 import Section from '@/components/ui-custom/Section';
